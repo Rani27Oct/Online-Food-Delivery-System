677ec0eaf71f6b57c12488d0f524b8e46b455957
Rani27Oct <ranipalle40@gmail.com>
2026-05-28 10:31:51 +0530

    Added FoodService.java

---
+++ FoodService.java
@@ -0,0 +1,8 @@
+public class FoodService {
+
+    public void showMenu() {
+        System.out.println("Pizza");
+        System.out.println("Burger");
+        System.out.println("Pasta");
+    }
+}
