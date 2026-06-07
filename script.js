23862bf545d0db2acb7487535f8d05933250d9ca
Rani27Oct <ranipalle40@gmail.com>
2026-06-07 21:36:54 +0530

    Enhance orderFood and add placeOrder, searchFood functions

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-function orderFood(foodName) {
-    alert(foodName + " ordered successfully!");
+function orderFood(foodName){
+    alert("✅ " + foodName + " added successfully!");
+}
+
+function placeOrder(){
+    alert("🎉 Order Placed Successfully!");
+}
+
+function searchFood(){
+
+    let input =
+    document.getElementById("searchBox")
+    .value.toLowerCase();
+
+    let cards =
+    document.getElementsByClassName("card");
+
+    for(let i=0;i<cards.length;i++){
+
+        let foodName =
+        cards[i].getElementsByTagName("h2")[0]
+        .innerText.toLowerCase();
+
+        if(foodName.includes(input)){
+            cards[i].style.display="block";
+        }
+        else{
+            cards[i].style.display="none";
+        }
+    }
 }
