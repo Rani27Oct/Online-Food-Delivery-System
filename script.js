8c7800e6b74d192916ac2fe50ff6a1ad9362e8a7
Rani27Oct <ranipalle40@gmail.com>
2026-06-07 21:49:14 +0530

    Improve code readability in script.js
    Refactor code for better readability by formatting lines and removing unnecessary alerts.

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-alert("JavaScript Loaded Successfully!");
-
 function orderFood(foodName){
     alert("✅ " + foodName + " added successfully!");
 }
 
 function placeOrder(){
     alert("🎉 Order Placed Successfully!");
+
+    let inputs = document.querySelectorAll("input");
+
+    inputs.forEach(input=>{
+        input.value="";
+    });
 }
 
 function searchFood(){
 
-    let input = document.getElementById("searchBox").value.toLowerCase();
+    let input =
+    document.getElementById("searchBox")
+    .value.toLowerCase();
 
-    let cards = document.getElementsByClassName("card");
+    let cards =
+    document.getElementsByClassName("card");
 
     for(let i=0;i<cards.length;i++){
 
-        let foodName = cards[i]
-            .getElementsByTagName("h2")[0]
-            .innerText.toLowerCase();
+        let foodName =
+        cards[i].getElementsByTagName("h2")[0]
+        .innerText.toLowerCase();
 
         if(foodName.includes(input)){
             cards[i].style.display="block";
